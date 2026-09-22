275c47aaf1975b4b36d2218359093e3bcde13f18
Luca <lucaalvarezkellick@gmail.com>
2026-09-22 18:50:44 +0800

    Updated error handling for _init_driver #2

--- src/imu_handler/imu_handler/imu_node.py
+++ src/imu_handler/imu_handler/imu_node.py
@@ -2,7 +2,6 @@ from geometry_msgs.msg import Vector3Stamped
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
-import sys
 
 from .mpu6500_driver import MPU6500Driver
 #from .bmi088_driver import BMI088Driver  # (Import secondary drivers here)
@@ -65,7 +64,6 @@ class IMUNode(Node):
                     return
                 except Exception as e:
                     probe_errors[name] = str(e)
-                    sys.exit(1)
 
             msg = f'Could not auto-detect any connected IMU. Probe logs: {probe_errors}'
             self.get_logger().fatal(msg)
